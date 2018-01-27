3082eb810045dfc58a27038f25fc281c4bc60843
Abi Perez <abi.sevilla09@gmail.com>
2018-01-27 06:24:10 -0600

    routing and api controller for first end point

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -1,15 +1,20 @@
-
 <?php
-// src/AppBundle/Controller/DefaultController.php
- 
 namespace AppBundle\Controller;
  
+use AppBundle\Entity\AreasConocimiento;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
- 
-class DefaultController extends FOSRestController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+
+
+class ApiController extends FOSRestController
 {
     /**
      * @Rest\Get("/")
@@ -19,5 +24,30 @@ class DefaultController extends FOSRestController
         $data = ['hello' => 'world'];
         $view = $this->view($data, Response::HTTP_OK);
         return $view;
+    }
+
+    //Funcion que utilizamos para devolver una petición incorrecta
+    private function badRequest($msg,$help=null)
+    {
+      return array(
+        'mensage'=>$msg,
+        'help'=>$help
+      );
+    }
+
+
+     //Esta acción devuelve todas las areas de conocimiento
+    public function GetAreasDeConocimientoAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:AreasConocimiento');
+        $empresas = $repository->findAll();
+        //var_dump($empresas);
+        $data = array('areas_conocimiento' => array());
+        foreach ($empresas as $empresa) {
+            $data['areas_conocimiento'][] = $this->serializeEmpresa($empresa);
+        }
+        $response = new JsonResponse($data, 200);
+        return $response;
+        //return $this->json($empresas);*/
     }
 }
